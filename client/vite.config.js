ca354ba85c3107adbc43206485e2e750b4568980
M.Arshad <arshadm1512@gmail.com>
2025-06-07 22:46:05 +0530

    vite.config.js update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: ['https://convohub-k3t8.onrender.com'], 
+  },
 })
